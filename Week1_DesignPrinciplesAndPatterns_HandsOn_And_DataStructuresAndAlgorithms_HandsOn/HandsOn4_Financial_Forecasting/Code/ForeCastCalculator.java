3527fe7331e50b203a9e772e73406a41425509cc
Vinay08-dev <vinays8114@gmail.com>
2025-06-20 16:43:59 +0530

    Add files via upload

---
+++ Week1_DesignPrinciplesAndPatterns_HandsOn_And_DataStructuresAndAlgorithms_HandsOn/HandsOn4_Financial_Forecasting/Code/ForeCastCalculator.java
@@ -0,0 +1,23 @@
+package Forecast;
+
+
+public class ForeCastCalculator {
+
+    public static double predictFutureValue(double currentValue, double growthRate, int years) {
+        if (years == 0) {
+            return currentValue;
+        }
+        return predictFutureValue(currentValue * (1 + growthRate), growthRate, years - 1);
+    }
+
+    public static void main(String[] args) {
+        double initialValue = 10000;
+        double annualGrowthRate = 0.10; 
+        int forecastYears = 5;
+
+        double futureValue = predictFutureValue(initialValue, annualGrowthRate, forecastYears);
+        System.out.printf("Future Value after %d years: %.2f\n", forecastYears, futureValue);
+    }
+}
+
+
